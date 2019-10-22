da33102b5c23fdd85be673bb47c6f7c94fc8c817
jose E Nieva Toppa <jose.ent17@gmail.com>
2019-10-22 20:17:34 -0300

    test aleatorio 1 prueba

---
+++ js/testAleatorio.js
@@ -0,0 +1,44 @@
+
+function resultado() {
+    var p1,p2 ,p3, p4, p5,nota;
+
+    //pregunt 1
+    if(document.getElementById('p1').checked==true){
+        p1=1
+    }
+
+    //pregunt 2
+    if(document.getElementById('p2').checked==true){
+        p2=1
+    }
+
+    //pregunt 3
+    if(document.getElementById('p3').checked==true){
+        p3=1
+    }
+
+    //pregunt 4
+    if(document.getElementById('p4').checked==true){
+        p4=1
+    }
+
+    //pregunt 5
+    if(document.getElementById('p5').checked==true){
+        p5=1}
+        else {p6=0}
+        
+    
+    //pregunt 6
+    if(document.getElementById('p6').checked==true){
+        p6=1}
+        else {p6=0}
+
+
+}
+
+
+//var rangoSuperior = 100;
+//var rangoInferior = 0;
+//var aleatorio = Math.floor(Math.random()*(rangoSuperior-(rangoInferior-1))) + rangoInferior;
+
+//alert(aleatorio);
