e2fa0c5faf6bad0a233987150aeefac9e088f2f5
jose E Nieva Toppa <jose.ent17@gmail.com>
2020-05-29 21:53:38 -0300

    teacherscreate

--- routes/Teachers.js
+++ routes/Teachers.js
@@ -5,27 +5,26 @@ const Schema = mongoose.Schema;
 const ObjectID = mongoose.Types.ObjectId;
 const bcryptjs = require("bcryptjs");
 
-const encriptarPassWord = password =>{
-const saltos = bcryptjs.genSaltSync(2);
-const passwordEncriptado = bcryptjs.hashSync(password,saltos);
-return passwordEncriptado
+const encriptarPassWord = (password) => {
+  const saltos = bcryptjs.genSaltSync(2);
+  const passwordEncriptado = bcryptjs.hashSync(password, saltos);
+  return passwordEncriptado;
 };
 
-const compararPassword = (userPassword, hashPasswordDb) => bcryptjs.compareSync(userPassword, hashPasswordDb)
+const compararPassword = (userPassword, hashPasswordDb) =>
+  bcryptjs.compareSync(userPassword, hashPasswordDb);
 
 const TeachersSchema = new Schema({
   _id: ObjectID,
   usuario: {
     type: String,
-    lowercase: true,
     required: [true, "can't be blank"],
     match: [/^[a-zA-Z0-9]+$/, "is invalid"],
     index: true,
-    
   },
   contrasena: {
-    type:String,
-    required: [true, "can't be blank"]
+    type: String,
+    required: [true, "can't be blank"],
   },
   nombre: String,
   apellido: String,
@@ -34,24 +33,19 @@ const TeachersSchema = new Schema({
     required: true,
     minlength: 8,
     maxlength: 8,
-    
   },
   mail: {
     type: String,
-    lowercase: true,
     required: [true, "can't be blank"],
     match: [/\S+@\S+\.\S+/, "is invalid"],
     index: true,
   },
   admin: Boolean,
-  image: String
+  image: String,
 });
-
 
 // metodos
 const TeachersModel = mongoose.model("teachers", TeachersSchema);
-
-
 
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
@@ -74,25 +68,24 @@ router.get("/", async (req, res) => {
 
 // registrar teacher
 router.post("/", async (req, res) => {
-   console.log("entrando a registrar");
-   
+  console.log("entrando a registrar");
+
   const teacherNuevo = {
     _id: new ObjectID(),
-    usuario:req.body.usuario,
-    contrasena:req.body.contrasena,
+    usuario: req.body.usuario,
+    contrasena: req.body.contrasena,
     nombre: req.body.nombre,
     apellido: req.body.apellido,
     dni: req.body.dni,
     mail: req.body.mail,
-    admin: req.body.admin
+    admin: req.body.admin,
   };
 
-teacherNuevo.contrasena = encriptarPassWord(req.body.contrasena);
-console.log(teacherNuevo.contrasena);
+  teacherNuevo.contrasena = encriptarPassWord(req.body.contrasena);
+  console.log(teacherNuevo.contrasena);
 
-
-try {
-  const teacher = new TeachersModel(teacherNuevo);
+  try {
+    const teacher = new TeachersModel(teacherNuevo);
     const respuesta = await teacher.save();
     res.json({
       mensaje: "teacher registrado correctamente",
@@ -100,35 +93,28 @@ try {
     });
   } catch (error) {
     console.log(error);
-    
+
     res.status(500).json({ mensaje: "error al crear teacher", tipo: error });
   }
 });
 
-
-
-
 // logear teacher
 router.post("/login", async (req, res) => {
   try {
-    const {usuario,contrasena}= req.body;
-    const doc = await TeachersModel.find({usuario});
+    const { usuario, contrasena } = req.body;
+    const doc = await TeachersModel.find({ usuario });
     const hashDb = doc[0].contrasena;
-    const esContrasenaCorrecta = compararPassword(contrasena, hashDb)
-   
+    const esContrasenaCorrecta = compararPassword(contrasena, hashDb);
+
     if (esContrasenaCorrecta) {
-      res.json({mensaje: "ingresado correctamente", respuesta: doc})
-    }else {
+      res.json({ mensaje: "ingresado correctamente", respuesta: doc });
+    } else {
       res.status(401).json({ mensaje: "contraseña incorrecta" });
     }
-
   } catch (error) {
     res.status(500).json({ mensaje: "error al conectar teacher", tipo: error });
   }
 });
-
-
-
 
 router.put("/:id", async (req, res) => {
   const id = req.params.id;
@@ -144,10 +130,10 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", async(req, res) => {
+router.delete("/:id", async (req, res) => {
   const id = req.params.id;
   try {
-    const respuesta =  await TeachersModel.findByIdAndDelete(id);
+    const respuesta = await TeachersModel.findByIdAndDelete(id);
     res.json({ mensaje: "usuario borrado", usuario: respuesta });
   } catch (error) {
     res.status(500).json({ mensaje: "error", tipo: error });
